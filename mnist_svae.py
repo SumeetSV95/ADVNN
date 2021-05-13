830e991d4263a36fc143ba67e6d45b9787ceea78
SumeetSV95 <70956253+SumeetSV95@users.noreply.github.com>
2021-05-13 17:29:10 -0400

    commit

--- mnist_svae.py
+++ mnist_svae.py
@@ -1,11 +1,11 @@
-
-  
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL
 import tensorflow as tf
 import tensorflow_probability as tfp
 import time
+
+tfd = tfp.distributions
 
 
 class CVAE(tf.keras.Model):
@@ -34,13 +34,12 @@ class CVAE(tf.keras.Model):
 
         ])
         self.discriminator = tf.keras.Sequential([
-          tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
-          tf.keras.layers.Dense(units=256, activation='relu'),
-          tf.keras.layers.Dense(units=128, activation='relu'),  
-          tf.keras.layers.Dense(units=64,  activation='relu'),
-          tf.keras.layers.Dense(units=1, activation='relu'),                               
+            tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+            tf.keras.layers.Dense(units=64, activation='relu'),
+            tf.keras.layers.Dense(units=32, activation='relu'),
+            tf.keras.layers.Dense(units=32, activation='relu'),
+            tf.keras.layers.Dense(units=1, activation='relu'),
         ])
-       
 
         self.decoder = tf.keras.Sequential(
             [
@@ -58,10 +57,6 @@ class CVAE(tf.keras.Model):
                     filters=1, kernel_size=3, strides=1, padding='same'),
             ]
         )
-      
-
-       
-
 
     @tf.function
     def sample(self, eps=None):
@@ -88,6 +83,9 @@ class CVAE(tf.keras.Model):
         y_hat = self.classifier(z)
         probs = tf.sigmoid(y_hat)
         return probs
+
+    def discriminate(self, z):
+        return self.discriminator(z)
 
 
 def preprocess_images(images):
@@ -140,6 +138,17 @@ def compute_loss(model, x, y):
     return -tf.reduce_mean(logpx_z + logpz - logqz_x + class_loss)
 
 
+def computeLossDiscriminator(model, x):
+    mean, logvar = model.encode(x)
+    z = model.reparameterize(mean, logvar)
+    dist = tfd.Normal(loc=np.zeros(model.latent_dim), scale=np.ones(model.latent_dim))
+    z_fake = dist.sample([len(x)])
+    d_real = model.discriminate(z)
+    d_fake = model.discriminate(z_fake)
+
+    return -tf.reduce_mean(tf.math.log(tf.sigmoid(d_real)) + tf.math.log(1 - tf.sigmoid(d_fake)))
+
+
 @tf.function
 def train_step(model, x, optimizer, y):
     """Executes one training step and returns the loss.
@@ -148,17 +157,28 @@ def train_step(model, x, optimizer, y):
   """
     with tf.GradientTape() as tape:
         loss = compute_loss(model, x, y)
+
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 
+@tf.function
+def train_discriminator_step(model, x, optimizer):
+    """Executes one training step and returns the loss.
+  This function computes the loss and gradients, and uses the latter to
+  update the model's parameters.
+  """
+    with tf.GradientTape() as tape:
+        loss = computeLossDiscriminator(model, x)
 
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 
 def main():
     optimizer = tf.keras.optimizers.Adam(1e-4)
     train_dataset, test_dataset = loadData()
-    epochs = 10
+    epochs = 2
     # set the dimensionality of the latent space to a plane for visualization later
     latent_dim = 2
     num_examples_to_generate = 16
@@ -169,11 +189,11 @@ def main():
         shape=[num_examples_to_generate, latent_dim])
     model = CVAE(latent_dim)
     # generate_and_save_images(model, 0, test_sample)
-
+    bestElbo = float('-inf')
+    bestEpoch = -1
     for epoch in range(1, epochs + 1):
         start_time = time.time()
         for train_x, train_labels in train_dataset:
-
             train_step(model, train_x, optimizer, train_labels)
         end_time = time.time()
 
@@ -181,10 +201,43 @@ def main():
         for test_x, test_labels in test_dataset:
             loss(compute_loss(model, test_x, test_labels))
         elbo = -loss.result()
+        if elbo>bestElbo:
+            bestElbo = elbo
+            bestEpoch = epoch
+            model.save_weights("bestModels/weights_{}.h5".format(epoch))
         # display.clear_output(wait=False)
         print('Epoch: {}, Test set ELBO: {}, time elapse for current epoch: {}'
               .format(epoch, elbo, end_time - start_time))
         # generate_and_save_images(model, epoch, test_sample)
+    print("loading weights...")
+    #model.load_weights("bestModels/weights_{}.h5".format(bestEpoch))
+
+    print("loading done...")
+    for layer in model.encoder.layers:
+        layer.trainable = False
+    for layer in model.decoder.layers:
+        layer.trainable = False
+    for layer in model.classifier.layers:
+        layer.trainable = False
+    bestEpoch = -1
+    bestloss = float('inf')
+    for epoch in range(1, epochs + 1):
+        start_time = time.time()
+        for train_x, train_labels in train_dataset:
+            train_discriminator_step(model, train_x, optimizer)
+        end_time = time.time()
+        loss = tf.keras.metrics.Mean()
+        for test_x, test_labels in test_dataset:
+            loss(computeLossDiscriminator(model, test_x))
+        currLoss = loss.result()
+        if currLoss< bestloss:
+            bestloss = currLoss
+            bestEpoch = epoch
+            model.save_weights("bestModels/weights_{}.h5".format(epoch))
+
+        # display.clear_output(wait=False)
+        print('Epoch: {}, Test set ELBO: {}, time elapse for current epoch: {}'
+              .format(epoch, currLoss, end_time - start_time))
 
 
 if __name__ == "__main__":
