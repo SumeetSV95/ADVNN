106b301135b817c143d7a70c2f1ee77e419605b4
SumeetSV95 <70956253+SumeetSV95@users.noreply.github.com>
2021-05-14 03:35:11 -0400

    modified

--- MNIST_Train.py
+++ MNIST_Train.py
@@ -3,18 +3,30 @@ from tensorflow import keras
 from keras.datasets import mnist
 import numpy as np
 
-MNIST_Classifier = keras.Sequential([
-tf.keras.layers.Dense(units=756,use_bias=True,activation='relu'),
-tf.keras.layers.Dense(units=100,use_bias=True,activation='relu'),
-tf.keras.layers.Dense(units=10,use_bias=True)
-])
-MNIST_Classifier.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
-(x_train, y_train),(x_test,y_test)=mnist.load_data()
-x_train = x_train.reshape(-1, 28*28)
-x_test = x_test.reshape(-1, 28*28)
-x_train = x_train.astype(np.float)/255
-x_test = x_test.astype(np.float)/255
 
-MNIST_Classifier.fit(x=x_train,y=y_train,batch_size=32,epochs=10,validation_data=(x_test,y_test))
+def preprocess_images(images):
+    images = images.reshape((images.shape[0], 28, 28, 1)) / 255.
+    return np.where(images > .5, 1.0, 0.0).astype('float32')
+
+MNIST_Classifier = tf.keras.Sequential([
+    tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
+
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+MNIST_Classifier.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
+                         loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+x_train = preprocess_images(x_train)
+x_test = preprocess_images(x_test)
+
+
+MNIST_Classifier.fit(x=x_train, y=y_train, batch_size=32, epochs=10, validation_data=(x_test, y_test))
 
 MNIST_Classifier.save("MNIST_Model")
